6ef00435f1e1ff74014c340b2f9dcfdf76ce2850
brunamfguimaraes <brunamfguimaraes@gmail.com>
2023-02-27 15:57:11 -0300

    Fix: Corrigindo conflitos de merge

--- lib/blogs_api_web/controllers/post_controller.ex
+++ lib/blogs_api_web/controllers/post_controller.ex
@@ -28,7 +28,7 @@ defmodule BlogsApiWeb.PostController do
 
   def delete(conn, %{"id" => id}) do
     id
-    |> BlogsApi.post_delete()
+    |> BlogsApi.delete_post()
     |> handle_delete(conn)
   end
 
